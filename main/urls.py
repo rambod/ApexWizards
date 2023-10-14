ad96b624c1f79ccccc430dbfe5c8675826081a95
Rambod Ghashghai <gh.rambod@gmail.com>
2023-10-14 22:52:40 +0330

    test

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path
-from .views import main
+from .views import main , test_page
 
 urlpatterns = [
-    path('', main, name="main"),    
+    path('', main, name="main"),  
+    path('test/', test_page),  
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def main(request):
     return render(request, 'main/index.html')
+
+def test_page(request):
+    context = {
+        'test' : [1,2,3,4,5,6],
+    }
+    return render(request, 'main/test.html',context=context)
